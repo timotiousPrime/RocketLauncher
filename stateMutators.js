243f49441baf9249eb22ac9a1e24c5452446d848
timotiousPrime <timodb031@gmail.com>
2021-07-10 21:15:21 +0200

    set state mutators and created object constructor for falling objects

---
+++ stateMutators.js
@@ -0,0 +1,65 @@
+const gameState = {
+    basketPos: 50,
+    basketValue: 0,
+    score: 0,
+    livesRemaining: 3,
+    gameLevel: 1,
+    gameMode: 'running',
+    fallingObjects: [],
+}
+
+const setBasketPos = (state, value) => ({
+    ...state,
+    basketPos: value,
+});
+
+const setBasketValue = (state, value) => ({
+    ...state,
+    basketValue: value,
+});
+
+const setScore = (state, value) => ({
+    ...state,
+    score: value,
+});
+
+const setlivesRemaining = (state, value) => ({
+    ...state,
+    livesRemaining: value,
+});
+
+const setGameLevel = (state, value) => ({
+    ...state,
+    gameLevel: value,
+});
+
+const setGameMode = (state, value) => ({
+    ...state,
+    gameMode: value,
+});
+
+// Object constructor for falling object
+function FallingObject(posX, posY, numerator, denominator) {
+    this.posX = posX;
+    this.posY = posY;
+    this.numerator = numerator;
+    this.denominator = denominator;
+    this.value = numerator / denominator
+}
+
+function setFallingObjPosX (fallingObj, value) {
+    fallingObj.posX = value;
+}
+
+const setFallingObjPosY = (fallingObj, value) => {
+    fallingObj.posY = value;
+}
+
+
+const setFallingObjNumerator = (fallingObj, value) => {
+    fallingObj.numerator = value;
+}
+
+const setFallingObjDenominator = (fallingObj, value) => {
+    fallingObj.denominator = value;
+}
