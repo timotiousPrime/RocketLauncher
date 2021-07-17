624b6e35416a5b6dba61e8300cbcbfee19281ce3
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-07-17 22:57:48 +0700

    Remove explicit deps from stateful logic class
    - make it more generic and have specific behaviors passed down
    - will next do a game main function where it sets everything up in one place

--- statefulLogic.js
+++ statefulLogic.js
@@ -1,20 +1,21 @@
-import { INIT_STATE } from './constants'
-import { renderGame } from './rendering'
-
 export class StatefulLogic {
     state
+    getInitState
+    onStateUpdate
 
-    constructor() {
+    constructor(dependencies) {
+        this.getInitState = dependencies.getInitState
+        this.onStateUpdate = dependencies.onStateUpdate
         this.resetState()
     }
 
     resetState() {
-        this.state = { ...INIT_STATE }
+        this.state = this.getInitState()
     }
 
     mutate(mutatorFn, ...args) {
         const nextState = mutatorFn(this.state, ...args)
-        renderGame(nextState)
+        this.onStateUpdate(nextState)
         this.state = nextState
     }
 }
