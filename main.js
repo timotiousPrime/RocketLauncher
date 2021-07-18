4ef0c3370654d293cfdf23e91bfcfc457b1c6ae2
timotiousPrime <timodb031@gmail.com>
2021-07-18 11:02:30 +0200

    Added setupEventListeners fn in main file

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 import { renderGame } from './rendering'
 import { StatefulLogic } from './statefulLogic'
 import { INIT_STATE } from './constants'
+import { setupEventListener } from './EventListeners'
 
 function runGame() {
     const logic = new StatefulLogic({
         onStateUpdate: renderGame,
         getInitState: () => ({ ...INIT_STATE }),
     })
-
+    
     // TODO: Set up event hanlders
-    // setupEventHandlers(logic)
+    setupEventListener(logic)
 
     // TODO: Start rain
     // rain(logic)
