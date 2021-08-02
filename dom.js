7281e0e16d55007332c206d57e333c9038dd848a
timotiousPrime <timodb031@gmail.com>
2021-08-02 22:13:58 +0200

    functions to play sound effects

--- dom.js
+++ dom.js
@@ -70,3 +70,60 @@ export function playBackgroundMusic(isMuted) {
     bgMusic.play()
     bgMusic.classList.add('musicPlaying')
 }
+
+export function movingBasketSound(isMuted) {
+    const basketSound = document.getElementById(EL_IDS.basketSound)
+    if (!basketSound) return
+    basketSound.muted = isMuted
+    basketSound.currentTime = 0
+    basketSound.play()
+}
+
+export function rocketTakeOffSound(isMuted) {
+    const rocketTakeOffSound = document.getElementById(EL_IDS.rocketTakeOffSound)
+    if (!rocketTakeOffSound) return
+    rocketTakeOffSound.muted = isMuted
+    rocketTakeOffSound.currentTime = 0
+    rocketTakeOffSound.play()
+}
+
+export function rocketExplodeSound(isMuted) {
+    const lifeLostSound = document.getElementById(EL_IDS.lifeLostSound)
+    if (!lifeLostSound) return
+    lifeLostSound.muted = isMuted
+    lifeLostSound.currentTime = 0
+    lifeLostSound.play()
+}
+
+
+export function levelUpSound(isMuted) {
+    const levelUpSound = document.getElementById(EL_IDS.levelUpSound)
+    if (!levelUpSound) return
+    levelUpSound.muted = isMuted
+    levelUpSound.currentTime = 0
+    levelUpSound.play()
+}
+
+export function SpawnFallingObjectSound(isMuted) {
+    const spawnFallingObjectSound = document.getElementById(EL_IDS.spawnFallingObjectSound)
+    if (!spawnFallingObjectSound) return
+    spawnFallingObjectSound.muted = isMuted
+    spawnFallingObjectSound.currentTime = 0
+    spawnFallingObjectSound.play()
+}
+
+export function catchFallingObjectSound(isMuted) {
+    const catchFallingObjectSound = document.getElementById(EL_IDS.catchFallingObjectSound)
+    if (!catchFallingObjectSound) return
+    catchFallingObjectSound.muted = isMuted
+    catchFallingObjectSound.currentTime = 0
+    catchFallingObjectSound.play()
+}
+
+export function gameOverSound(isMuted) {
+    const gameOverMusic = document.getElementById(EL_IDS.gameOverMusic)
+    if (!gameOverMusic) return
+    gameOverMusic.muted = isMuted
+    gameOverMusic.currentTime = 0
+    gameOverMusic.play()
+}
